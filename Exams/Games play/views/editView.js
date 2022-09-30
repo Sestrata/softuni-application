bfc70b7adddc39af1a7f1659f9074d20cc0b9a9a
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-09-30 12:02:47 +0300

    Create editView.js

---
+++ Exams/Games play/views/editView.js
@@ -0,0 +1,38 @@
+import { html } from '../util.js';
+import { get, put } from '../services/api.js';
+
+const template = (game, onSubmit) => html`
+<section id="edit-page" class="auth">
+            <form id="edit" @submit=${onSubmit}>
+                <div class="container">
+                    <h1>Edit Game</h1>
+                    <label for="leg-title">Legendary title:</label>
+                    <input type="text" id="title" name="title" value=${game.title}>
+                    <label for="category">Category:</label>
+                    <input type="text" id="category" name="category" value=${game.category}>
+                    <label for="levels">MaxLevel:</label>
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" value=${game.maxLevel}>
+                    <label for="game-img">Image:</label>
+                    <input type="text" id="imageUrl" name="imageUrl" value=${game.imageUrl}>
+                    <label for="summary">Summary:</label>
+                    <textarea name="summary" id="summary">${game.summary}</textarea>
+                    <input class="btn submit" type="submit" value="Edit Game">
+                </div>
+            </form>
+        </section>`;
+
+export async function editView(ctx) {
+    const game = await get('/data/games/' + ctx.params.id);
+    ctx.render(template(game, onSubmit));
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.target));
+        const verification = Object.values(data).some(e => e == '');
+        if (verification) {
+            return alert('All fields are required');
+        }
+        await put('/data/games/' + ctx.params.id, data);
+        ctx.page.redirect('/');
+    }
+}
